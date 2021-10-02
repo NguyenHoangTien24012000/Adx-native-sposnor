61880f8a94314716f410e549e9a868cb8cbe957c
NguyenTien2401 <hoangtienflytom@gmail.com>
2021-10-03 00:27:19 +0700

    day2

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+};
+
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+};
+
+function buildStyles() {
+    return gulp.src('./sass/**/*.scss')
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  };
