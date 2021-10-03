9f187cc1b6ead3c775c12e17fa3bf4ed67174932
NguyenTien2401 <hoangtienflytom@gmail.com>
2021-10-03 17:59:49 +0700

    Done Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,53 @@
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
 
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+// Sass task 
+function scssTask(){
+    return src('src/sass/style.scss', {sourcemaps: true})
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', {sourcemaps: '.'}));
+}
 
-function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
+//JavaScript task
+function jsTask(){
+    return src('src/js/script.js', {sourcemaps: true})
+        .pipe(terser())
+        .pipe(dest('dist',{sourcemaps: '.'}))
+}
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-};
 
-function buildStyles() {
-    return gulp.src('./sass/**/*.scss')
-      .pipe(sass.sync().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-  };
+// Browsersync tasks
+function browsersyncServe(cb){
+    browsersync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    cb();
+}
+
+function browsersyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+
+// Watch tasks
+function watchTask(){
+    watch('*.html', browsersyncReload);
+    watch(['src/sass/**/*.scss', 'src/js/**/*.js'], series(scssTask, jsTask, browsersyncReload))
+}
+
+
+// Default gulp task
+exports.default = series(
+    scssTask,
+    jsTask,
+    browsersyncServe,
+    watchTask
+)
+
